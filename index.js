1c865e00384fd8df3f7d5ea76933cfe171a85292
gradyasher <gradyasher@gmail.com>
2017-04-25 10:44:45 -0500

    final

--- index.js
+++ index.js
@@ -61,126 +61,108 @@ var questions = [{
 		}]
 	}]
 
-// question clickhandler
-	// get users answer
-	// compare answer to actual answer
-	// if they equal each other increment score counter
-	// if counter < questions.length
-		// loadQuestion()
-	// else
-		// hide .question
-		// Insert score into restart-message
-		// show .restart-message
-
-// restart clickhandler
-	// reset counter
-	// reset score
-	// hide restart-message
-	// show .question
-	//  loadQuestion()
-
 var counter = 0
 var score = 0
 var template = `<div>
 				<p id="question"></p>
 				<p>Choose one of the following:</p>
 				<form id="answers">
-					<ul class="list">
-						<li>
-							<label for="a1"></label>
-							<input type="radio" name="ans" value="1" />
-						</li>
-						<li>
-							<label for="a2"></label>
-							<input type="radio" name="ans" value="2" />
-						</li>
-						<li>
-							<label for="a3"></label>
-							<input type="radio" name="ans" value="3" />
-						</li>
-						<li>
-							<label for="a4"></label>
-							<input type="radio" name="ans" value="4" />
-						</li>
-					</ul>
+					
+					<p>
+						<input type="radio" name="ans" value="1" id="a1"/>
+						<label for="a1"></label>
+					</p>
+					<p>
+						<input type="radio" name="ans" value="2" id="a2"/>
+						<label for="a2"></label>
+					</p>
+					<p>
+						<input type="radio" name="ans" value="3" id="a3"/>
+						<label for="a3"></label>
+					</p>
+					<p>
+						<input type="radio" name="ans" value="4" id="a4"/>
+						<label for="a4"></label>
+					</p>
+					
 					<input id="submit" type="button" value="Answer"/>
 				</form>
 				</div>`
 
 function loadContent() {
+
 	if (counter === questions.length) {
 		$('.questions').hide()
 		$('.restart-message').show()
 		$('.score').append(` ${score} / ${counter}`)
 	}
+
 	else {
-	var toAppend = $(template)
-	toAppend.find("#question").text(questions[counter].question)
-	toAppend.find("label[for='a1']").text(Object.values(questions[counter].answers[0]))
-	toAppend.find("label[for='a2']").text(Object.values(questions[counter].answers[1]))
-	toAppend.find("label[for='a3']").text(Object.values(questions[counter].answers[2]))
-	toAppend.find("label[for='a4']").text(Object.values(questions[counter].answers[3]))
-	$('.questions').html(toAppend)
+		var toAppend = $(template)
+		toAppend.find("#question").text(questions[counter].question)
+		var answers = Object.values(questions[counter].answers)
+
+		answers.forEach(function(answer, i) {
+			toAppend.find(`label[for='a${i + 1}']`).text(answers[i].ans)
+		}) 
+
+		$('.questions').html(toAppend)
 	}
-}
-
-function correctAnswer() {
-	
-}
-
-
-function hideStart() {
-	$('#start-button').hide()
-	$('.start-message').hide()
 }
 
 function isCorrect() {
 	var checkedAnswer = $('input:checked')
-	console.log(checkedAnswer)
+	
 	if (checkedAnswer.length === 0) {
         alert("Please select an answer");
         event.preventDefault();
     }
+
     else {
         if (parseFloat(checkedAnswer.val()) === questions[counter].correct) {
-        	console.log('correct')
         	counter++
         	score++
         }
         else {
-        	console.log('incorrect')
         	counter++
         }
-
     }
 }
 
-function Restart() {
-	// $('#restart-button').hide()
-	$('.restart-message').hide()
-	$('.start-message').show()
-	$('#start-button').show()
-}
+// (rdr) => a+b 
+
+// const jesse = hello => hello
+
+// var jesse = function(hello) {
+// 	return hello
+// }
+
+// function jesse(hello) {
+// 	return hello
+// }
 
 $(function() {
 	$('.restart-message').hide()
 
 	$('#start-button').click(function(event) {
     	event.preventDefault();
-    	hideStart();
-    	loadContent();
+    	$('.questions').show();
+    	$('#start-button').hide()
+		$('.start-message').hide()    	
+		loadContent();
 	});
 
 	$(".questions").on("click", "#submit", function(event) {
-		// event.preventDefault();
 		isCorrect();
-		$('#start-button').click();
+		loadContent();
 	});
 
 	$('#restart-button').click(function(event) {
 		event.preventDefault();
-		counter = 1
+		counter = 0
 		score = 0
-		Restart();
+		$('.restart-message').hide()
+		$('.start-message').show()
+		$('#start-button').show()
 	})
 });
